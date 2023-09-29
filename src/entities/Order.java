19a4d799d8d4e24b1a5be0085b34c9e16e086964
fabio.griesang <fabio.griesang@outlook.com>
2023-09-29 10:36:21 -0300

    Exercício de orientação a objetos e composição de objetos em Java
    Implementação um programa de console conforme enunciado:
    Ler os dados de um pedido com N itens (N fornecido pelo usuário).
    Depois, mostrar um sumário do pedido conforme exemplo (próxima página).
    Nota: o instante do pedido deve ser o instante do sistema.

---
+++ src/entities/Order.java
@@ -0,0 +1,99 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+	private Date moment;
+	private OrderStatus status;
+	
+	//associações
+	private Client client;
+	
+	private List<OrderItem> items = new ArrayList<OrderItem>();
+	
+	public Order() {
+		
+	}
+
+	//ao gerar o contrutor, remover items, porque ele já está definido em List
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	//remover o método setItems e getItems porque ele é definido em lista
+	// forma de proteção e encapsulamento da lista
+	
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	public double total() {
+		double sum = 0.0;
+		for (OrderItem it : items) {
+			sum += it.subTotal();
+		}
+		return sum;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : items) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
+	}
+	
+	
+
+	
+	
+	
+	
+}
